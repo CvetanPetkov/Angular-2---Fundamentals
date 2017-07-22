0357a114bc0600e2e008a9adc3af58e32f4dbe0e
Cvetan Petkov <refref@abv.bg>
2017-07-22 11:08:15 +0300

    Refactored Solution - with Components

--- 02_Components_Markup/Exercise/src/app/app.module.ts
+++ 02_Components_Markup/Exercise/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NavigationComponent} from './navigation.component/navigation.component';
+import {PlaneDetailsComponent} from './plane-details.component/plane-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PlaneDetailsComponent
   ],
   imports: [
     BrowserModule
